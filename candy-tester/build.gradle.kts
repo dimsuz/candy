acc65e835b754de7c1a5e3ee992ed36a09c9d4c1
Dmitry Suzdalev <mail@dimsuz.ru>
2024-05-19 18:44:26 +0200

    Add very basic ui

--- candy-tester/build.gradle.kts
+++ candy-tester/build.gradle.kts
@@ -1,18 +1,21 @@
 plugins {
     kotlin("jvm")
-    id("org.jetbrains.compose") version "1.6.2"
-    application
+    id("org.jetbrains.compose") version "1.6.10-rc03"
 }
 
 kotlin {
-    jvmToolchain(15)
+    jvmToolchain(17)
 }
 
 dependencies {
     implementation(compose.desktop.currentOs)
     implementation(compose.materialIconsExtended)
+    implementation("io.github.bonede:tree-sitter:0.22.5a")
+    implementation("io.github.bonede:tree-sitter-kotlin:0.3.1")
 }
 
-application {
-    mainClass.set("ru.dimsuz.candy.tester.MainKt")
+compose.desktop {
+    application {
+        mainClass = "ru.dimsuz.candy.tester.MainKt"
+    }
 }
--- candy-tester/src/main/kotlin/ru/dimsuz/candy/tester/Main.kt
+++ candy-tester/src/main/kotlin/ru/dimsuz/candy/tester/Main.kt
@@ -1,4 +1,215 @@
+@file:Suppress("FunctionName")
+
 package ru.dimsuz.candy.tester
 
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.type
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import org.treesitter.TSParser
+import org.treesitter.TSTree
+import org.treesitter.TSTreeCursor
+import org.treesitter.TreeSitterKotlin
+import ru.dimsuz.candy.tester.uikit.VSpacer
+
+const val STARTING_SOURCE_CODE = """
+              class Rectangle(val height: Double, val length: Double) {
+                  val perimeter = (height + length) * 2 
+              }
+              
+              fun main() {
+                  val rectangle = Rectangle(5.0, 2.0)
+                  println("The perimeter is ${'$'}{rectangle.perimeter}")
+              }
+            """
+
+// TODO whenever edits are made, must inform the composition (add the parsed_at field?)
+/**
+ * A special wrapper class only to be able to mark stable, to reduce recompositions
+ */
+@Stable
+@JvmInline
+value class ParsedTree(
+  val value: TSTree,
+)
+
 fun main() {
+  val parser = TSParser()
+  val kotlin = TreeSitterKotlin()
+  parser.setLanguage(kotlin)
+
+  val tree = ParsedTree(parser.parseString(null, STARTING_SOURCE_CODE))
+
+  application {
+    Window(
+      onCloseRequest = ::exitApplication,
+      title = "Candy Tester",
+      state = rememberWindowState(
+        position = WindowPosition(Alignment.Center),
+        size = DpSize(1200.dp, 768.dp),
+      ),
+      onKeyEvent = { key ->
+        if (key.type == KeyEventType.KeyDown && key.key == Key.Escape) {
+          exitApplication()
+          true
+        } else false
+      }
+    ) {
+      var sourceCode by remember { mutableStateOf(TextFieldValue(STARTING_SOURCE_CODE.trimIndent())) }
+      MaterialTheme {
+        Row(
+          modifier = Modifier.fillMaxSize()
+        ) {
+          SourceCode(
+            modifier = Modifier
+              .padding(8.dp)
+              .weight(1f)
+              .fillMaxHeight(),
+            text = sourceCode,
+            onChanged = { sourceCode = it }
+          )
+          Column(
+            modifier = Modifier
+              .padding(8.dp)
+              .weight(1f)
+              .fillMaxHeight()
+          ) {
+            Ast(
+              modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.6f),
+              tree = tree
+            )
+            VSpacer(8.dp)
+            Box(
+              modifier = Modifier
+                .border(1.dp, MaterialTheme.colors.onBackground)
+                .weight(0.4f)
+                .padding(4.dp)
+            ) {
+              var queryState by remember { mutableStateOf(TextFieldValue("Query")) }
+              BasicTextField(
+                modifier = Modifier.fillMaxSize(),
+                value = queryState,
+                onValueChange = { queryState = it }
+              )
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+@Composable
+private fun SourceCode(
+  modifier: Modifier = Modifier,
+  text: TextFieldValue,
+  onChanged: (TextFieldValue) -> Unit
+) {
+  println("source code render")
+  Box(
+    modifier = modifier
+      .border(1.dp, MaterialTheme.colors.onBackground)
+      .padding(4.dp)
+      .verticalScroll(rememberScrollState())
+  ) {
+    BasicTextField(
+      modifier = Modifier.matchParentSize(),
+      value = text,
+      onValueChange = onChanged
+    )
+  }
+}
+
+@Composable
+private fun Ast(
+  modifier: Modifier = Modifier,
+  tree: ParsedTree,
+) {
+  println("ast render")
+  val cursor = remember(tree) { TSTreeCursor(tree.value.rootNode) }
+  cursor.reset(tree.value.rootNode)
+  Column(
+    modifier = modifier
+  ) {
+    Text(
+      "Syntax Tree"
+    )
+    VSpacer(4.dp)
+    // TODO make a LazyColumn?
+    Box(
+      modifier = Modifier
+        .border(1.dp, MaterialTheme.colors.onBackground)
+        .padding(4.dp)
+    ) {
+      val scrollState = rememberScrollState()
+      val scrollAdapter = rememberScrollbarAdapter(scrollState)
+      Column(
+        modifier = Modifier
+          .fillMaxWidth()
+          .verticalScroll(scrollState)
+      ) {
+        var indentLevel = 0
+        var retracing = false
+        var rootReached = false
+        while (!rootReached) {
+          if (!retracing && cursor.currentNode().isNamed) {
+            Text(
+              modifier = Modifier.offset(24.dp * indentLevel),
+              text = cursor.currentNode().type
+            )
+          }
+          if (!retracing && cursor.gotoFirstChild()) {
+            indentLevel += 1
+            continue
+          }
+
+          if (cursor.gotoNextSibling()) {
+            retracing = false
+            continue
+          }
+
+          if (cursor.gotoParent()) {
+            retracing = true
+            indentLevel -= 1
+          } else {
+            rootReached = true
+          }
+        }
+      }
+      VerticalScrollbar(scrollAdapter, Modifier.align(Alignment.TopEnd))
+    }
+  }
 }
